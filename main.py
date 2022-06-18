fc4b58e095693aa2fff4bdd8b60c849ebb7926c2
Maximilian1021 <Maximilian.Preger@gmx.de>
2022-06-18 02:17:27 +0200

    Format COde

--- main.py
+++ main.py
@@ -66,7 +66,8 @@ async def reload(ctx):
 async def on_command_error(ctx: commands.Context, error):
     errormsg = error
     embed = discord.Embed(title="Es ist ein Fehler aufgetreten", colour=discord.Colour.red(),
-                          description=f"Bei der Ausführung des Commands ist ein Fehler aufgetreten.\n **Error:** {errormsg}")
+                          description=f"Bei der Ausführung des Commands ist ein Fehler aufgetreten.\n "
+                                      f"**Error:** {errormsg}")
     await ctx.send(embed=embed)
 
 
