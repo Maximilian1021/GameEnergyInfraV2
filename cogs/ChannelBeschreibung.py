e0d38aebc5254819e7f6e5707fd8612da5aeec19
Maximilian1021 <Maximilian.Preger@gmx.de>
2022-07-05 22:07:07 +0200

    Format the Code

--- cogs/ChannelBeschreibung.py
+++ cogs/ChannelBeschreibung.py
@@ -10,17 +10,14 @@ class ChannelBeschreibungen(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_roles=True, ban_members=True)
     async def VorschlagPic(self, ctx):
-
         embed = discord.Embed(colour=discord.Colour.teal())
         embed.set_image(url="https://img.max1021.de/cooltext413443916933016.png")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
-
     @commands.command()
     @commands.has_permissions(manage_roles=True, ban_members=True)
     async def VorschlagDesc(self, ctx):
-
         embed = discord.Embed(title="Vorschlag einreichen!",
                               description="Dieser Channel ist für Vorschläge welche Fragen im FAQ aufgenommen werden "
                                           "sollen. Bitte schreibt eure Fragen und eine Mögliche Antwort in den Channel "
