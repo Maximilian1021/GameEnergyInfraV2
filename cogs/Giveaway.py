a054ed4aa0ae7179d9cae28806c5d35d2c76eb7a
Maximilian1021 <Maximilian.Preger@gmx.de>
2022-07-08 09:56:53 +0200

    Embed Erstellung und Auslosung

--- cogs/Giveaway.py
+++ cogs/Giveaway.py
@@ -2,10 +2,27 @@ import random
 import discord
 from discord.ext import commands
 
+time = "08.07.2022 - 11:00 Uhr"
+prize = "1 Random Steam Key"
+
+
+
 
 class Giveaway(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.has_permissions(manage_roles=True, ban_members=True)
+    @commands.command()
+    async def GiveawayCreate(self, ctx):
+        embed = discord.Embed(colour=discord.Colour.teal(), title="Neues Gewinnspiel", description="Reagiere mit "
+                                                                                                      ":tada: um teilzunehmen!")
+        embed.add_field(name="Preis", value="{}".format(prize), inline=False)
+        embed.add_field(name="Hosted by", value=f"{ctx.author.mention}", inline=False)
+        embed.add_field(name="Auslosung gegen", value=f"{time}")
+        msg = await ctx.send(embed=embed)
+        reactions = await msg.add_reaction("🎉")
+        await ctx.message.delete()
 
     @commands.slash_command()
     @commands.has_permissions(manage_guild=True)
@@ -21,7 +38,7 @@ class Giveaway(commands.Cog):
         info = message_link.split("/")
         try:
             if not int(info[4]) == ctx.guild.id:
-                raise commands.CommandError("Bitte gib eineen Link zu einer Nachricht in diesem Server an.")
+                raise commands.CommandError("Bitte gib einen Link zu einer Nachricht in diesem Server an.")
             message = await ctx.guild.get_channel(int(info[5])).fetch_message(int(info[6]))
         except IndexError or discord.NotFound:
             raise commands.CommandError("Bitte gib einen gültigen Link an")
@@ -35,6 +52,11 @@ class Giveaway(commands.Cog):
         if winners == 1:
             winner = random.choice(await reaction.users().flatten())
             await ctx.respond(f"Der Gewinner ist {winner.mention}")
+            embed = discord.Embed(title="Giveaway Auslosung :tada:",
+                                  description=f"Gewonnen haben folgende User: {winner.mention} \n **Herzlichen"
+                                              f" Glückwunsch** \n Bitte melde dich bei mir via Ticket (wenn du bereits"
+                                              f"eins offen hast, nutze dies hierfür!)")
+            await ctx.respond(embed=embed)
         else:
             users = await reaction.users().flatten()
             _winners = []
@@ -44,6 +66,11 @@ class Giveaway(commands.Cog):
                 users.remove(winner)
             _winners = [f"{i.mention}" for i in _winners]
             await ctx.respond(f"Die Gewinner sind {', '.join(_winners)}")
+            embed = discord.Embed(title="Giveaway Auslosung :tada:",
+                                  description=f"Gewonnen haben folgende User: {', '.join(_winners)}"
+                                              f"\n**Herzlichen Glückwunsch** \n Bitte meldet euch bei mir "
+                                              f"via Ticket (wenn du bereits eins offen hast, nutze dies hierfür!)")
+            await ctx.respond(embed=embed)
 
 
 def setup(bot):
