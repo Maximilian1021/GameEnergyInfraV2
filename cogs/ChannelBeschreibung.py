4de0111db0caf6079f66ed1b2b48a22011f5a76f
Maximilian1021 <Maximilian.Preger@gmx.de>
2022-08-20 23:43:41 +0200

    Added Urlaub Announcment

--- cogs/ChannelBeschreibung.py
+++ cogs/ChannelBeschreibung.py
@@ -31,5 +31,21 @@ class ChannelBeschreibungen(commands.Cog):
         await ctx.message.delete()
 
 
+    @commands.command()
+    @commands.has_permissions(manage_roles=True, ban_members=True)
+    async def URLAUB(self, ctx):
+        embed = discord.Embed(title="Urlaub vom 22.8 - 28.8 - Veränderter Support ",
+                              description="Lieber Member des Discords \n\n Für mich geht es ab Montag in den Urlaub. "
+                                          "In dieser Zeit kann ich keinen Support leisten und Server einrichten \n\n"
+                                          "Für alle bestehenden Server geht aber die aktuelle Aktivitätskontrolle und "
+                                          "der Support wie gewohnt weiter. Hierfür stehen euch <@572808505304809502> "
+                                          "und <@560491862880944139> zu Verfügung. (An alle anderen Bewerber ihr seid "
+                                          "schon noch im Rennen!) \n \n Ich bin für euch ab dem 29. August wieder da \n"
+                                          "\nVielen Dank für euer Verständnis und bis nächstes mal",
+                              colour=discord.Colour.teal())
+        await ctx.send(embed=embed)
+        await ctx.message.delete()
+
+
 def setup(bot: commands.Bot):
     bot.add_cog(ChannelBeschreibungen(bot))
