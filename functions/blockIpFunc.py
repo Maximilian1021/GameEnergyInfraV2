5a55799406c036e8016864002cf7d858c748845e
Maximilian1021 <Maximilian.Preger@gmx.de>
2023-06-27 08:07:54 +0200

    Added unblock Function

--- functions/blockIpFunc.py
+++ functions/blockIpFunc.py
@@ -32,3 +32,36 @@ def add_ip_to_docker_user_chain(server_ip, server_port, ssh_key_path, username, 
     ssh.close()
     print("Connection closed")
     print("------------")
+
+
+def remove_ip_from_docker_user_chain(server_ip, server_port, ssh_key_path, username, ip_to_remove):
+    # Erstellen eines SSH-Clients
+    ssh = paramiko.SSHClient()
+    # Automatisches Hinzufügen von Server-Schlüsseln
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Verbindung zum SSH-Server herstellen
+    try:
+        ssh.connect(server_ip, port=server_port, username=username, key_filename=ssh_key_path)
+        print("------------")
+        print(f"Connected to Server {server_ip}")
+    except Exception as e:
+        print(f"Error connecting to server: {e}")
+        return
+
+    # Prüfen, ob die IP-Adresse in der PREROUTING-Kette der Raw-Tabelle ist
+    check_command = "sudo iptables -t raw -L PREROUTING -n"
+    stdin, stdout, stderr = ssh.exec_command(check_command)
+    output = stdout.read().decode()
+    if ip_to_remove not in output:
+        print(f"The IP address {ip_to_remove} is not in the PREROUTING chain in the raw table on Server {server_ip}")
+    else:
+        # Entfernen der IP-Adresse aus der PREROUTING-Kette der Raw-Tabelle
+        remove_command = f"sudo iptables -t raw -D PREROUTING -s {ip_to_remove} -j DROP"
+        stdin, stdout, stderr = ssh.exec_command(remove_command)
+        print(f"IP address {ip_to_remove} has been removed from the PREROUTING chain in the raw table on Server {server_ip}.")
+
+    # SSH-Verbindung schließen
+    ssh.close()
+    print("Connection closed")
+    print("------------")
